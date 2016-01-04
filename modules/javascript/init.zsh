89ab9e8fb5db18bce478d78f88b98333323356eb
Alex Bepple <alex@bepple.de>
2016-01-04 16:46:36 +0100

    + nvm

--- modules/javascript/init.zsh
+++ modules/javascript/init.zsh
@@ -3,3 +3,6 @@ pmodload 'path'
 add_to_path "/usr/local/share/npm/bin"
 
 alias npm_lazy_start='npm_lazy --config ~/.config/npm_lazy.js'
+
+export NVM_DIR="/Users/ab/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
